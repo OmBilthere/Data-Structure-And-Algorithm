d4ef05b0d6591ab71be0bf0b2413ac10b7b748bb
Om Bilthere <ombilthere953@gmail.com>
2026-03-31 10:24:29 +0530

    Selection Sort

---
+++ SortingAlgo/SelectionSort.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void selectionSort(vector<int> &arr) {
+        
+        int n = arr.size();
+    
+        for(int i=0 ; i<n-1 ; i++) {
+             
+           int mini = i;
+            
+            for(int j=i+1 ; j<n; j++) {
+                
+               if( arr[mini] > arr[j]) 
+                   
+               mini = j;
+                
+            }
+        
+           
+            swap(arr[mini], arr[i]);
+        }
+    }
+};
