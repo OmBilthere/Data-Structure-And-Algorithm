a6558e1480980c954ee5a38c286594978c2181ca
Om Bilthere <ombilthere953@gmail.com>
2026-04-10 21:55:43 +0530

    single Number in Array

---
+++ Array/SingleNumber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int number = 0;
+
+        for(int i=0 ; i< n ; i++) {
+            
+          number = number ^ nums[i];
+
+        }
+
+
+        return number;
+
+    }
+};
