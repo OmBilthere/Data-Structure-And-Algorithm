85a9278386b5bd95db52f8960731f7c3663d4640
Om Bilthere <ombilthere953@gmail.com>
2026-04-17 21:17:05 +0530

    Max Subarray Sum

---
+++ Array/MaxSubarraySum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   // kadanes algo
+    int maxSubArray(vector<int>& nums) {
+        
+        int maxi = -1e9;
+
+        int sum = 0;
+
+        for(int i = 0 ; i < nums.size() ; i++) {
+            
+            sum += nums[i];
+
+            if(sum > maxi ) {
+
+                maxi = sum;
+            }   
+            
+           if (sum < 0) sum = 0;
+          
+        }
+
+        return maxi;
+    }
+};
